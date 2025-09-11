48ca4ad11d4d8916ca5d17932206a1a40e2b9ae1
Tsubasa Yamada <yamatsuba0325@gmail.com>
2025-09-11 11:05:24 +0900

    vercel用にデプロイ方式変更

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-
-  // ★ ESLint をビルド時は無視
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   // ★ TypeScript の型エラーもビルドは通す
   typescript: {
     ignoreBuildErrors: true,
   },
   basePath: "/webapp/CREW",
-  trailingSlash: true,
 };
 export default nextConfig;
